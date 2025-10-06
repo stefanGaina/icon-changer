62eaa0d680132837d9d3a607f683255f9aef1cfb
Gaina Stefan <stefan.gaina01@gmail.com>
2025-10-06 12:06:12 +0300

    Add tests for icon.cpp

--- src/icon.cpp
+++ src/icon.cpp
@@ -17,7 +17,7 @@
 
 #include "icon.hpp"
 
-#include <print>
+#include <format>
 #include <cassert>
 
 #include "logger.hpp"
@@ -28,6 +28,40 @@
 
 namespace icon_changer
 {
+
+icon::icon(const std::string_view file_path)
+	: resource_header{}
+	, resource_entries{}
+	, images{}
+{
+	std::ifstream                 file    = open_file(file_path);
+	const std::vector<icon_entry> entries = read_icon_entries(file);
+
+	read_images(file, entries);
+	convert_entries(entries);
+}
+
+std::vector<std::uint8_t> icon::get_header() const
+{
+	std::vector<std::uint8_t> serialized_header = {};
+	std::vector<std::uint8_t> header_bytes      = serialize(resource_header);
+	std::vector<std::uint8_t> entry_bytes       = {};
+
+	serialized_header.insert(serialized_header.end(), header_bytes.begin(), header_bytes.end());
+
+	for (const entry& entry : resource_entries)
+	{
+		entry_bytes = serialize(entry);
+		serialized_header.insert(serialized_header.end(), entry_bytes.begin(), entry_bytes.end());
+	}
+
+	return serialized_header;
+}
+
+std::vector<std::vector<std::uint8_t>>& icon::get_images() noexcept
+{
+	return images;
+}
 
 std::ifstream icon::open_file(const std::string_view file_path)
 {
@@ -64,14 +98,14 @@ std::vector<std::uint8_t> icon::serialize(const icon::entry& entry)
 
 void icon::read_header(std::ifstream& file)
 {
-	static constexpr std::uint16_t ICO_IMAGE_TYPE = 1;
-	static constexpr std::uint16_t CUR_IMAGE_TYPE = 2;
+	static constexpr std::uint16_t ICO_IMAGE_TYPE = 0x0001;
+	static constexpr std::uint16_t CUR_IMAGE_TYPE = 0x0002;
 
 	file.read(reinterpret_cast<char*>(&resource_header), sizeof(resource_header));
 
-	if (0 != resource_header.reserved)
+	if (0x0000 != resource_header.reserved)
 	{
-		throw std::invalid_argument{ std::format("Header reserved bytes are 0x{:X}, expecting 0!", resource_header.reserved) };
+		throw std::invalid_argument{ std::format("Header reserved bytes are 0x{:X}, expecting 0x{:X}!", resource_header.reserved, 0x0000) };
 	}
 
 	if (CUR_IMAGE_TYPE == resource_header.type)
@@ -82,6 +116,11 @@ void icon::read_header(std::ifstream& file)
 	if (ICO_IMAGE_TYPE != resource_header.type)
 	{
 		throw std::invalid_argument{ std::format("Image type 0x{:X} is invalid!", resource_header.type) };
+	}
+
+	if (0x0000 == resource_header.entries_count)
+	{
+		throw std::invalid_argument{ "Icon does not have image entries!" };
 	}
 }
 
@@ -103,14 +142,14 @@ void icon::read_images(std::ifstream& file, const std::vector<icon_entry>& entri
 
 	for (const icon_entry& entry : entries)
 	{
-		if (0 != entry.reserved)
+		if (0x00 != entry.reserved)
 		{
-			throw std::invalid_argument{ std::format("Entry's reserved byte is 0x{:X}, excepting 0!", entry.reserved) };
+			throw std::invalid_argument{ std::format("Entry's reserved byte is 0x{:X}, excepting 0x{:X}!", entry.reserved, 0x00) };
 		}
 
-		if (0 != entry.planes && 1 != entry.planes)
+		if (0x0000 != entry.planes && 0x0001 != entry.planes)
 		{
-			throw std::invalid_argument{ std::format("Entry's color planes is 0x{:X}, expecting 0 or 1!", entry.planes) };
+			throw std::invalid_argument{ std::format("Entry's color planes is 0x{:X}, expecting 0x{:X} or 0x{:X}!", entry.planes, 0x0000, 0x0001) };
 		}
 
 		image.resize(entry.image_size);
@@ -150,40 +189,6 @@ void icon::convert_entries(const std::vector<icon_entry>& entries)
 
 		resource_entries.push_back(std::move(entry));
 	}
-}
-
-icon::icon(const std::string_view file_path)
-	: resource_header{}
-	, resource_entries{}
-	, images{}
-{
-	std::ifstream                 file    = open_file(file_path);
-	const std::vector<icon_entry> entries = read_icon_entries(file);
-
-	read_images(file, entries);
-	convert_entries(entries);
-}
-
-std::vector<std::uint8_t> icon::get_header() const
-{
-	std::vector<std::uint8_t> serialized_header = {};
-	std::vector<std::uint8_t> header_bytes      = serialize(resource_header);
-	std::vector<std::uint8_t> entry_bytes       = {};
-
-	serialized_header.insert(serialized_header.end(), header_bytes.begin(), header_bytes.end());
-
-	for (const entry& entry : resource_entries)
-	{
-		entry_bytes = serialize(entry);
-		serialized_header.insert(serialized_header.end(), entry_bytes.begin(), entry_bytes.end());
-	}
-
-	return serialized_header;
-}
-
-std::vector<std::vector<std::uint8_t>>& icon::get_images() noexcept
-{
-	return images;
 }
 
 } // namespace icon_changer
